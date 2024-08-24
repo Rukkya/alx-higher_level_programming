825b07c0c03895bedf864c33ab46155409d6711e
rukkya <rokiaossama@gmail.com>
2024-08-24 09:59:12 +0300

    ;

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// Get command-line arguments (excluding the first two default arguments)
+const args = process.argv.slice(2);
+
+// Check if there are any arguments
+if (args[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
