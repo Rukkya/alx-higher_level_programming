2f4654dca0416279118fe5bb438ac177a0009e22
rukkya <rokiaossama@gmail.com>
2024-08-24 11:27:26 +0300

    ;

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Array of strings to be printed
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Loop through the array and print each line
+lines.forEach(line => console.log(line));
