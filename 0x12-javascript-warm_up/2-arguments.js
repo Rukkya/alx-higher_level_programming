e7af6132bd843d5a3f588b385a28501af1343868
rukkya <rokiaossama@gmail.com>
2024-08-24 09:49:40 +0300

    ;

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+// Get the number of arguments passed to the script (excluding the first two default arguments)
+const args = process.argv.slice(2); // `process.argv` contains the command-line arguments
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
