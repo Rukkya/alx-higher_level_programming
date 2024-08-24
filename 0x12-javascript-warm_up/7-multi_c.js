daa76a2d53603d29ea01718b1d2cf13b8ffc9dba
rukkya <rokiaossama@gmail.com>
2024-08-24 11:32:56 +0300

    ;

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,7 +7,7 @@ const args = process.argv.slice(2);
 const x = Number(args[0]);
 
 // Use a single console.log for the error message and a loop to print the message
-if (isNaN(x) || x <= 0) {
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
