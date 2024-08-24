2e0bdaf856f6ee305d475df498ee5e5cbf4b8c84
rukkya <rokiaossama@gmail.com>
2024-08-24 11:29:21 +0300

    ;

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// Get command-line arguments (excluding the first two default arguments)
+const args = process.argv.slice(2);
+
+// Extract the first argument and attempt to convert it to an integer
+const x = Number(args[0]);
+
+// Use a single console.log for the error message and a loop to print the message
+if (isNaN(x) || x <= 0) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
