1214a02c48bee80af48e80ddca44ac15d8457782
rukkya <rokiaossama@gmail.com>
2024-08-25 04:54:22 +0300

    ;

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Define an empty class Rectangle
+class Rectangle {
+  // No properties or methods defined
+}
+
+// Export the class so it can be used in other scripts
+module.exports = Rectangle;
